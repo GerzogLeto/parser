1ce4740483472879cd42ab4910a5faddc60fbbce
gerzog_leto <stepfano@yandex.ru>
2021-08-18 02:24:52 +0400

    add method getLinks()

--- src/App.java
+++ src/App.java
@@ -1,11 +1,15 @@
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class App {
     public static void main(String[] args) {
         URL url = null;
         try {
-            url = new URL("https://aambassador.com/images/car/");
+            url = new URL("http://scar3crow.com/wallpapers/");
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
@@ -16,9 +20,20 @@ public class App {
             while ((temp = bufferedReader.readLine()) != null){
                 resource += temp;
             }
-            System.out.println(resource);
+            //<a href="1210836281769.jpg">
+            String reg = "<a href=" + "\".*?\\.jpg\"";
+            List<String> links = getLinks(resource, Pattern.compile(reg));
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static List<String> getLinks (String res, Pattern pattern){
+        List<String> list = new ArrayList<>();
+        Matcher matcher = pattern.matcher(res);
+        while (matcher.find()){
+            list.add(res.substring(matcher.start(), matcher.end()));
+        }
+        return list;
     }
 }
